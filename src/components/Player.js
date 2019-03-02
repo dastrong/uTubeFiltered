aa6e5cf61bbd429ba52b637835870809677d54af
dastrong <danielstrong.ds@gmail.com>
2019-03-02 20:08:50 +0900

    added placeholder for videos

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,45 +1,68 @@
 import React from "react";
+import PropTypes from "prop-types";
 import YouTube from "react-youtube";
+import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-// import { connect } from "react-redux";
+const opts = {
+  width: "100%",
+  playerVars: {
+    autoplay: 1,
+    iv_load_policy: 3,
+    rel: 0
+  }
+};
 
-function _onFinish(e) {
-	console.log("Video is Finished; Remove from Playlist");
-}
+const styles = {
+  compContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    boxShadow: "inset 0px 0px 3px 0.5px rgba(101, 98, 98, 0.91)"
+  },
+  iFrameContainer: {
+    width: "100%",
+    height: 360,
+    transition: "opacity 0.45s",
+    opacity: 0
+  },
+  iFrameIsLoaded: {
+    opacity: 1
+  },
+  spinner: {
+    position: "absolute"
+  }
+};
 
-// -X-this component renders an iFrame player containing the playlist given
-// give the user an option to delete videos once they've been watched
-// default should be auto delete
-// it should listen and remove videos that have been watched
-// show a subscribe button to the playing video's channel
-// show a delete video from playlist button
-// show an add to saved watch later playlist
-// since watch later is not supported anymore
-// maybe I should create a Saved Videos playlist when user clicks this button
-// if user has already done this and has a saved videos playlist, skip the creation part
-// show the playing videos statistic and info
-
-const Player = props => (
-	<Grid item xs={12} sm={10} md={6}>
-		<YouTube
-			opts={{
-				width: "100%",
-				maxWidth: "700px",
-				playerVars: {
-					autoplay: 1,
-					iv_load_policy: 3,
-					rel: 0,
-					listType: "playlist",
-					list: "PL81JazZ9FQVqXpP5iZ9enh5ewa4aq_-G3",
-				},
-			}}
-			onEnd={_onFinish}
-			// onStateChange={this.consoleIt}
-			// onPlaybackRateChange={this.consoleIt}
-			// onPlaybackQualityChange={this.consoleIt}
-		/>
-	</Grid>
+const Player = ({
+  classes,
+  videoId,
+  onFinish,
+  isPlayerReady,
+  togglePlayerReady
+}) => (
+  <Grid item className={classes.compContainer}>
+    <YouTube
+      containerClassName={`${classes.iFrameContainer} ${
+        isPlayerReady ? classes.iFrameIsLoaded : ""
+      }`}
+      videoId={videoId}
+      opts={opts}
+      onEnd={onFinish}
+      onReady={togglePlayerReady}
+    />
+    {!isPlayerReady && <CircularProgress className={classes.spinner} />}
+  </Grid>
 );
 
-export default Player;
+Player.propTypes = {
+  classes: PropTypes.object.isRequired,
+  videoId: PropTypes.string.isRequired,
+  onFinish: PropTypes.func.isRequired,
+  togglePlayerReady: PropTypes.func.isRequired,
+  isPlayerReady: PropTypes.bool.isRequired
+};
+
+export default withStyles(styles)(Player);
