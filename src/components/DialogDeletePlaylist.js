5d447f0bc91901f7e836406207bf57db59f00576
Daniel Strong <danielstrong.ds@gmail.com>
2019-08-22 11:25:12 +0900

    made delete dialog fullscreen for mobile to avoid cramping

--- src/components/DialogDeletePlaylist.js
+++ src/components/DialogDeletePlaylist.js
@@ -1,27 +1,30 @@
 import React from "react";
-import Dialog from "@material-ui/core/Dialog";
+import { Dialog, useMediaQuery } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 import DialogBasicInner from "./DialogBasicInner";
 
 export default function DialogDeletePlaylist(props) {
-  const { open, negFunc, posFunc } = props;
+	const { open, negFunc, posFunc } = props;
+	const theme = useTheme();
+	const fullScreen = useMediaQuery(theme.breakpoints.down("xs"));
 
-  return (
-    <Dialog
-      open={open}
-      fullWidth
-      onClose={negFunc}
-      aria-labelledby="delete-playlist-title"
-      aria-describedby="delete-playlist-text"
-    >
-      <DialogBasicInner
-        name="delete-playlist"
-        title="Are you sure you want to delete this playlist?"
-        msg="There's no undoing this..."
-        negText="Nvm, take me back."
-        posText="Yes, I'm sure."
-        negFunc={negFunc}
-        posFunc={posFunc}
-      />
-    </Dialog>
-  );
+	return (
+		<Dialog
+			fullScreen={fullScreen}
+			open={open}
+			onClose={negFunc}
+			aria-labelledby="delete-playlist-title"
+			aria-describedby="delete-playlist-text"
+		>
+			<DialogBasicInner
+				name="delete-playlist"
+				title="Are you sure you want to delete this playlist?"
+				msg="There's no undoing this..."
+				negText="Nvm, take me back."
+				posText="Yes, I'm sure."
+				negFunc={negFunc}
+				posFunc={posFunc}
+			/>
+		</Dialog>
+	);
 }
