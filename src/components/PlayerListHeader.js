55eab32c18e51ac5a21837abbd613e5b8a8e1151
Daniel Strong <danielstrong.ds@gmail.com>
2019-08-23 20:45:04 +0900

    save user autoDelete preference in localStorage

--- src/components/PlayerListHeader.js
+++ src/components/PlayerListHeader.js
@@ -6,55 +6,60 @@ import CheckIcon from "@material-ui/icons/CheckCircleRounded";
 import { toggleAutoDelete } from "../store/actions/ui";
 
 const useStyles = makeStyles(theme => ({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "10px 10px 0",
-    [theme.breakpoints.up("sm")]: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-      padding: "5px 10px"
-    }
-  },
-  title: {
-    fontSize: "1rem"
-  },
-  autoDelete: {
-    fontSize: "0.75rem"
-  }
+	container: {
+		display: "flex",
+		justifyContent: "center",
+		flexDirection: "column",
+		alignItems: "center",
+		padding: "10px 10px 0",
+		[theme.breakpoints.up("sm")]: {
+			flexDirection: "row",
+			alignItems: "center",
+			justifyContent: "space-between",
+			padding: "5px 10px"
+		}
+	},
+	title: {
+		fontSize: "1rem"
+	},
+	autoDelete: {
+		fontSize: "0.75rem"
+	}
 }));
 
 export default function PlayerListHeader(props) {
-  const { playlistTitle, autoDelete, storePatch } = props;
-  const classes = useStyles();
+	const { playlistTitle, autoDelete, storePatch } = props;
+	const classes = useStyles();
 
-  return (
-    <div className={classes.container}>
-      <Typography className={classes.title} variant="subtitle2">
-        {playlistTitle}
-      </Typography>
-      <FormControlLabel
-        className={classes.autoDelete}
-        control={
-          <Switch
-            checked={autoDelete}
-            checkedIcon={<CheckIcon />}
-            color="primary"
-            onChange={() => storePatch(toggleAutoDelete())}
-          />
-        }
-        label="Auto Delete"
-        labelPlacement="start"
-      />
-    </div>
-  );
+	function handleChange() {
+		localStorage.setItem("autoDelete", !autoDelete);
+		storePatch(toggleAutoDelete());
+	}
+
+	return (
+		<div className={classes.container}>
+			<Typography className={classes.title} variant="subtitle2">
+				{playlistTitle}
+			</Typography>
+			<FormControlLabel
+				className={classes.autoDelete}
+				control={
+					<Switch
+						checked={autoDelete}
+						checkedIcon={<CheckIcon />}
+						color="primary"
+						onChange={handleChange}
+					/>
+				}
+				label="Auto Delete"
+				labelPlacement="start"
+			/>
+		</div>
+	);
 }
 
 PlayerListHeader.propTypes = {
-  playlistTitle: PropTypes.string.isRequired,
-  autoDelete: PropTypes.bool.isRequired,
-  storePatch: PropTypes.func.isRequired
+	playlistTitle: PropTypes.string.isRequired,
+	autoDelete: PropTypes.bool.isRequired,
+	storePatch: PropTypes.func.isRequired
 };
