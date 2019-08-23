5aa97e0b95540d4c7b438e8f5b320c770b48acb9
Daniel Strong <danielstrong.ds@gmail.com>
2019-08-23 19:29:07 +0900

    saved with prettier

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,8 @@ export default () => {
 		rootReducer,
 		compose(
 			applyMiddleware(thunk),
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+				window.__REDUX_DEVTOOLS_EXTENSION__()
 		)
 	);
 };
