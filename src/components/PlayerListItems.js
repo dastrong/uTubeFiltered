2a87a1f38e0abf474f2e79bbe355eff56b75a9b5
DaStrong <danielstrong.ds@gmail.com>
2019-08-27 15:46:43 +0900

    passed inline funcs down to child

--- src/components/PlayerListItems.js
+++ src/components/PlayerListItems.js
@@ -1,48 +1,48 @@
 import React, { useLayoutEffect } from "react";
 import PropTypes from "prop-types";
 import PlayerListItem from "./PlayerListItem";
-import { plItemPlay } from "../store/actions/ids";
 
 export default function PlayerListItems(props) {
-	const {
-		videos,
-		deleteVid,
-		storePatch,
-		curPlItemId,
-		curVidIdx,
-		deletingId
-	} = props;
+  const {
+    videos,
+    deleteVid,
+    storePatch,
+    curPlItemId,
+    curVidIdx,
+    deletingId
+  } = props;
 
-	useLayoutEffect(() => {
-		document.querySelector("#items-holder").scrollTop = curVidIdx * 100;
-	}, [curVidIdx]);
+  useLayoutEffect(() => {
+    document.querySelector("#items-holder").scrollTop = curVidIdx * 100;
+  }, [curVidIdx]);
 
-	return (
-		<div id="items-holder" style={{ maxHeight: "500px", overflowY: "auto" }}>
-			{videos.map(({ videoTitle, thumbnail, playlistItemId }) => {
-				const isPlaying = playlistItemId === curPlItemId;
-				const isDeleting = playlistItemId === deletingId;
-				return (
-					<PlayerListItem
-						key={"listItem: " + playlistItemId}
-						title={videoTitle}
-						thumbnail={thumbnail}
-						deleteVid={() => deleteVid(isPlaying, playlistItemId)}
-						playVid={() => storePatch(plItemPlay(playlistItemId))}
-						isPlaying={isPlaying}
-						isDeleting={isDeleting}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div id="items-holder" style={{ maxHeight: "500px", overflowY: "auto" }}>
+      {videos.map(({ videoTitle, thumbnail, playlistItemId }) => {
+        const isPlaying = playlistItemId === curPlItemId;
+        const isDeleting = playlistItemId === deletingId;
+        return (
+          <PlayerListItem
+            key={"listItem: " + playlistItemId}
+            title={videoTitle}
+            thumbnail={thumbnail}
+            deleteVid={deleteVid}
+            storePatch={storePatch}
+            isPlaying={isPlaying}
+            isDeleting={isDeleting}
+            playlistItemId={playlistItemId}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 PlayerListItems.propTypes = {
-	videos: PropTypes.array.isRequired,
-	deleteVid: PropTypes.func.isRequired,
-	storePatch: PropTypes.func.isRequired,
-	curPlItemId: PropTypes.string.isRequired,
-	curVidIdx: PropTypes.number.isRequired,
-	deletingId: PropTypes.string.isRequired
+  videos: PropTypes.array.isRequired,
+  deleteVid: PropTypes.func.isRequired,
+  storePatch: PropTypes.func.isRequired,
+  curPlItemId: PropTypes.string.isRequired,
+  curVidIdx: PropTypes.number.isRequired,
+  deletingId: PropTypes.string.isRequired
 };
