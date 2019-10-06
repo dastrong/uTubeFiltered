2e24a3ee279ffd174d9683f3e8e56d310e94323b
Daniel Strong <danielstrong.ds@gmail.com>
2019-10-06 18:31:51 +0900

    fix the devtools for dev and prob

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
-	return createStore(
-		rootReducer,
-		compose(
-			applyMiddleware(thunk)
-			// window.__REDUX_DEVTOOLS_EXTENSION__ &&
-			// 	window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
-	);
+	return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
