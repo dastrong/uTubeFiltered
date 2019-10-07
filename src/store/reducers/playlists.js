311c3fd64793697554951cc508a7b74ec7429038
Daniel Strong <danielstrong.ds@gmail.com>
2019-10-07 19:57:08 +0900

    can now delete videos in nonUTF playlists

--- src/store/reducers/playlists.js
+++ src/store/reducers/playlists.js
@@ -1,57 +1,59 @@
 import {
-  GET_PLAYLISTS,
-  CREATE_PLAYLIST,
-  DELETE_PLAYLIST,
-  UPDATE_PLAYLIST,
-  GET_PLAYLIST_ITEMS,
-  DELETE_PLAYLIST_ITEM
+	GET_PLAYLISTS,
+	CREATE_PLAYLIST,
+	DELETE_PLAYLIST,
+	UPDATE_PLAYLIST,
+	GET_PLAYLIST_ITEMS,
+	DELETE_PLAYLIST_ITEM
 } from "../actionTypes";
 
 export default (state = [], action) => {
-  const {
-    type,
-    playlists,
-    newPlaylist,
-    tags,
-    id,
-    items,
-    playlistItemId,
-    playlistId
-  } = action;
-  switch (type) {
-    case GET_PLAYLISTS:
-      return playlists;
-    case CREATE_PLAYLIST:
-      return [...state, newPlaylist];
-    case DELETE_PLAYLIST:
-      return state.filter(playlist => playlist.id !== id);
-    case UPDATE_PLAYLIST:
-      return state.map(playlist =>
-        playlist.id === id
-          ? {
-              ...playlist,
-              items: playlist.items.map(item => ({ ...item })),
-              tags
-            }
-          : playlist
-      );
-    case GET_PLAYLIST_ITEMS:
-      return state.map(playlist =>
-        playlist.id === id ? { ...playlist, items } : playlist
-      );
-    case DELETE_PLAYLIST_ITEM:
-      return state.map(playlist =>
-        playlist.id === playlistId
-          ? {
-              ...playlist,
-              tags: { ...playlist.tags, channels: [...playlist.tags.channels] },
-              items: playlist.items.filter(
-                item => item.playlistItemId !== playlistItemId
-              )
-            }
-          : playlist
-      );
-    default:
-      return state;
-  }
+	const {
+		type,
+		playlists,
+		newPlaylist,
+		tags,
+		id,
+		items,
+		playlistItemId,
+		playlistId
+	} = action;
+	switch (type) {
+		case GET_PLAYLISTS:
+			return playlists;
+		case CREATE_PLAYLIST:
+			return [...state, newPlaylist];
+		case DELETE_PLAYLIST:
+			return state.filter(playlist => playlist.id !== id);
+		case UPDATE_PLAYLIST:
+			return state.map(playlist =>
+				playlist.id === id
+					? {
+							...playlist,
+							items: playlist.items.map(item => ({ ...item })),
+							tags
+					  }
+					: playlist
+			);
+		case GET_PLAYLIST_ITEMS:
+			return state.map(playlist =>
+				playlist.id === id ? { ...playlist, items } : playlist
+			);
+		case DELETE_PLAYLIST_ITEM:
+			return state.map(playlist =>
+				playlist.id === playlistId
+					? {
+							...playlist,
+							tags: playlist.tags
+								? { ...playlist.tags, channels: [...playlist.tags.channels] }
+								: null,
+							items: playlist.items.filter(
+								item => item.playlistItemId !== playlistItemId
+							)
+					  }
+					: playlist
+			);
+		default:
+			return state;
+	}
 };
