602edf6eb10f6a57fba806b5f353af47226e6926
Daniel Strong <danielstrong.ds@gmail.com>
2020-03-18 19:42:37 +0900

    updated destructuring keys due to SDK changes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -4,46 +4,46 @@ import { showSnackBar } from "./snacks";
 import { setTab } from "./ui";
 
 export const setCurrentUser = user => ({
-	type: SET_CURRENT_USER,
-	user
+  type: SET_CURRENT_USER,
+  user
 });
 
 export const logoutUser = () => ({ type: LOGOUT_USER });
 
 export function authUser(cb) {
-	return async dispatch => {
-		try {
-			const user = await cb();
-			const [userDetails, tokenDetails] = [user.w3, user.Zi];
-			const userData = {
-				name: userDetails.ig,
-				email: userDetails.U3,
-				image: userDetails.Paa,
-				tokenAccess: tokenDetails.access_token,
-				tokenId: tokenDetails.id_token,
-				tokenType: tokenDetails.token_type
-			};
-			dispatch(setCurrentUser(userData));
-			// send the user to their playlists
-			dispatch(setTab(1));
-			// grab the user's playlists (thunk)
-			dispatch(getPlaylists(userData.tokenAccess));
-		} catch (err) {
-			console.log(err);
-			dispatch(showSnackBar("error", "An error occured [Login]"));
-		}
-	};
+  return async dispatch => {
+    try {
+      const user = await cb();
+      const [userDetails, tokenDetails] = [user.Qt, user.uc];
+      const userData = {
+        name: userDetails.Ad,
+        email: userDetails.zu,
+        image: userDetails.UK,
+        tokenAccess: tokenDetails.access_token,
+        tokenId: tokenDetails.id_token,
+        tokenType: tokenDetails.token_type
+      };
+      dispatch(setCurrentUser(userData));
+      // send the user to their playlists
+      dispatch(setTab(1));
+      // grab the user's playlists (thunk)
+      dispatch(getPlaylists(userData.tokenAccess));
+    } catch (err) {
+      console.log(err);
+      dispatch(showSnackBar("error", "An error occured [Login]"));
+    }
+  };
 }
 
 export function unAuthUser(cb) {
-	return async dispatch => {
-		try {
-			localStorage.clear();
-			await cb();
-			dispatch(logoutUser());
-		} catch (err) {
-			console.log(err);
-			dispatch(showSnackBar("error", "An error occured [Logout]"));
-		}
-	};
+  return async dispatch => {
+    try {
+      localStorage.clear();
+      await cb();
+      dispatch(logoutUser());
+    } catch (err) {
+      console.log(err);
+      dispatch(showSnackBar("error", "An error occured [Logout]"));
+    }
+  };
 }
